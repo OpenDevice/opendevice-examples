bf5c764036160b75a49ef5c024022a38f950b3ce
ricardojlrufino <ricardo.jl.rufino@gmail.com>
2015-08-30 12:36:06 -0300

    update webresources location

--- opendevice-tutorial/src/main/java/WebSocketDemo.java
+++ opendevice-tutorial/src/main/java/WebSocketDemo.java
@@ -1,7 +1,11 @@
 import br.com.criativasoft.opendevice.connection.IWSServerConnection;
 import br.com.criativasoft.opendevice.connection.discovery.DiscoveryService;
 import br.com.criativasoft.opendevice.core.LocalDeviceManager;
-import br.com.criativasoft.opendevice.core.connection.Connections;
+import br.com.criativasoft.opendevice.core.model.Device;
+import br.com.criativasoft.opendevice.core.model.DeviceType;
+import br.com.criativasoft.opendevice.core.model.Sensor;
+
+import java.io.File;
 
 
 /**
@@ -21,17 +25,23 @@ public class WebSocketDemo extends LocalDeviceManager {
 
     public WebSocketDemo() throws Exception {
 
+//        addDevice(new Device(1, "BLUE", DeviceType.DIGITAL));
+//        addDevice(new Device(2, "YELLOW", DeviceType.DIGITAL));
+//        addDevice(new Device(3, "RED", DeviceType.DIGITAL));
+//        addDevice(new Sensor(4, "SW1", DeviceType.DIGITAL));
+//        addDevice(new Device(5,"SW2", DeviceType.DIGITAL));
+
         // setup connection with arduino/hardware
-        addOutput(Connections.out.bluetooth("00:11:06:14:04:57")); // Connect to first USB port available
-        //addOutput(Connections.out.bluetooth("00:13:03:14:19:07"));
+//        addOutput(out.bluetooth("00:11:06:14:04:57"));
+        addOutput(out.usb()); // Connect to first USB port available
+
+        String path = getClass().getResource("/").getPath();
+        String current = new File(path).getParentFile().getParent();
 
         // Configure a Websocket interface for receiving commands over HTTP
-        IWSServerConnection server = Connections.in.websocket(8181);
-        // Static WebResources
-        String current = System.getProperty("user.dir");
-        System.out.println("Current Directory: " + current);
-        server.addWebResource( current + "/src/main/resources/webapp");
-        server.addWebResource( current + "/target/classes/webapp"); //  running exec:java
+        IWSServerConnection server = in.websocket(8181);
+        server.addWebResource(current + "/src/main/resources/webapp");
+        server.addWebResource(current + "/target/classes/webapp"); //  running exec:java
 
         addInput(server);
 

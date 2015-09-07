3fb3022f0a48379cc9e874c441a6dd83c05b76e6
ricardo.jl.rufino@gmail.com <ricardo.jl.rufino@gmail.com>
2015-09-06 21:19:14 -0300

    opendevice-3d-blender example

---
+++ opendevice-3d-blender/README.md
@@ -0,0 +1,44 @@
+opendevice-3d-blender
+==========================
+
+Sample application illustrating the integration between [OpenDevice][http://opendevice.io] and Blender  
+
+![OpenDevice_Blender](./assets/splash.png)
+
+Dependencies: 
+
+- Blender ( [install blender on linux](http://tipsonubuntu.com/2015/04/03/install-blender-2-74-ubuntu-14-04linux-mint-17) )
+- Java 6 (or higher)
+- Maven  
+
+
+Getting Started
+
+  1. Install OpenDevice Python library:
+
+    - Clone: https://github.com/OpenDevice/opendevice-extensions
+
+    - Follow the instructions (Basically clone and install like any other library python) 
+
+  2. Clone this repo into a new directory.
+
+  3. Compile: 
+  
+    - You can use pre-compiled version (opendevice-3d-blender-app.jar) or compile following the instructions:
+    
+    - Run on terminal `./run.sh` or `mvn package`,  it will compile and run the project
+    
+    - If not execute, check the ./target folder if the file exists: `opendevice-3d-blender-app.jar`
+    
+  4. Run OpenDevice (GUI): 
+
+    - Run `./run.sh` or `java -jar opendevice-3d-blender-app.jar`
+
+  5. Open Blender and import project: `./opendevice-3d-blender/assets/OpenDeviceHand.blend` 
+
+  6. On Blender, change the 'lib_path' variable in script 'HandsConn.py'
+
+  7. Put in Game Engine view and click in 'Start Game Engine': 
+    
+    - Changes on sliders in OpenDevice (GUI) must change the fingers (TIP: check console logs)
+
---
+++ opendevice-3d-blender/src/main/java/SlidersForm.java
@@ -0,0 +1,100 @@
+/*
+ * *****************************************************************************
+ * Copyright (c) 2013-2014 CriativaSoft (www.criativasoft.com.br)
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Ricardo JL Rufino - Initial API and Implementation
+ * *****************************************************************************
+ */
+
+import br.com.criativasoft.opendevice.connection.server.TCPServerConnection;
+import br.com.criativasoft.opendevice.core.LocalDeviceManager;
+import br.com.criativasoft.opendevice.core.model.Device;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Ricardo JL Rufino
+ * @date 05/09/15
+ */
+public class SlidersForm {
+    private JPanel rootPanel;
+    private List<JSlider> sliders;
+    private List<JLabel> labels;
+
+    private JSlider slider4;
+    private JSlider slider5;
+    private JSlider slider3;
+    private JSlider slider2;
+    private JSlider slider1;
+    private JLabel lab1;
+    private JLabel lab2;
+    private JLabel lab3;
+    private JLabel lab4;
+    private JLabel lab5;
+
+    private LocalDeviceManager manager= new LocalDeviceManager();
+
+    List<Device> devices;
+    Device finger1 = new Device(10, Device.ANALOG);
+    Device finger2 = new Device(11, Device.ANALOG);
+    Device finger3 = new Device(12, Device.ANALOG);
+    Device finger4 = new Device(13, Device.ANALOG);
+    Device finger5 = new Device(14, Device.ANALOG);
+
+    public SlidersForm() {
+
+        manager.addInput(new TCPServerConnection(8182));
+
+        sliders = Arrays.asList(slider1, slider2, slider3, slider4, slider5);
+        labels = Arrays.asList(lab1, lab2, lab3, lab4, lab5);
+        devices = Arrays.asList(finger1, finger2,finger3, finger4, finger5);
+
+        for (JSlider slider : sliders) {
+            slider.setValue(0);
+            slider.setMinimum(0);
+            slider.setMaximum(360);
+            slider.addChangeListener(changeListener);
+        }
+
+        try {
+            manager.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private ChangeListener changeListener = new ChangeListener() {
+        public void stateChanged(ChangeEvent e) {
+            JSlider source = (JSlider)e.getSource();
+            int indexOf = sliders.indexOf(source);
+
+            Device device = devices.get(indexOf);
+            JLabel label = labels.get(indexOf);
+            device.setValue(source.getValue());
+            label.setText(Integer.toString(source.getValue()));
+        }
+    };
+
+
+    public static void main(String[] args) throws Exception {
+        UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        JFrame frame = new JFrame("SlidersForm");
+        frame.setContentPane(new SlidersForm().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+
+}
